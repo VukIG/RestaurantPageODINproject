7633d43ba5430fdfaaecd72b681a2e9d087bd45e
VukIG <vuk.ignjatovic05@gmail.com>
2022-11-19 14:01:19 -0900

    epsum

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let nav=document.createElement('nav');
+nav.innerHTML=`
+<img src="https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png" alt="">
+<ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Contact</a></li>
+</ul>
+`
